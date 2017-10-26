d52c8030ab269ccd2138b0bc17d8965161d29509
aditReza <lete.note3@gmail.com>
2017-10-26 23:31:08 +0700

    release 2 add route

--- models/cities.js
+++ models/cities.js
@@ -6,21 +6,52 @@ class Cities{
     this.data_cities = []
   }
 
+  saveFile(data){
+    // console.log('+++++',data);
+    let to_JSON = JSON.stringify(data)
+    console.log('?????', to_JSON);
+    fs.writeFile('./data/data.json', to_JSON,(err)=>{
+      if(err){
+        console.log(err);
+      }
+    })
+  }
+
   getFile(cb){
     fs.readFile(this.file, 'utf8',(err,data)=>{
       if(err){
         console.log(err);
       }else{
+        console.log(data);
         let parse = JSON.parse(data)
-        // console.log(parse['cities']);
+
+        // this.data_cities.push(parse)
         cb(parse)
       }
+    })
+  }
+
+  create(submit,cb){
+    this.getFile((data)=>{
+      // console.log('<<<<<<<',data.cities,'>>>>>>>');
+      let last_index = []
+      for(let i=0; i<data.cities.length; i++){
+        last_index.push(data.cities[data.cities.length-1].id_cities)
+      }
+
+      data.cities.push({
+        id_cities : (data.cities.length == last_index[0]) ? (data.cities.length + 1) : (last_index[0] + 1),
+        name      : submit['name'],
+        province  : submit['province'],
+      })
+      this.saveFile(data.cities)
+      cb(data)
     })
   }
 
 }
 
 let cities = new Cities('./data/data.json')
-// cities.getFile()
+// cities.saveFile()
 
 module.exports = cities
--- routers/cities.js
+++ routers/cities.js
@@ -2,7 +2,18 @@ const express = require('express')
 const router  = express.Router()
 const model   = require('../models/cities')
 
+// CREATE
+router.get('/add',(req,res)=>{
+    res.render('cities-add')
+})
 
+router.post('/add',(req,res)=>{
+    model.create({name: req.body.name_cities, province: req.body.province_cities},()=>{
+      res.redirect('/cities')
+    })
+})
+
+// READ
 router.get('/',(req,res)=>{
   model.getFile((cities)=>{
     res.render('cities',cities)
