137f31dcf830c188c2ed741c2f39eab114e3e85a
wisnudj <jayalahwisnu@gmail.com>
2017-10-26 16:01:33 +0700

    release 0

--- app.js
+++ app.js
@@ -1,46 +1,28 @@
-/** EXPRESS FROM SCRATCH V.0
----------------------------
-Buatlah sebuah aplikasi sederhana menggunakan Express JS
-- Release 0
-Buatlah 3 routing yang memenuhi spesifikasi berikut ini :
-URL --> http://localhost:3000/
-menampilkan "Welcome to Express My App [Nama_Kalian]"
-URL --> http://localhost:3000/users
-menampilkan data users berupa object. di release ini object dibuat manual,
-silakan menentukan property dan value dari object tersebut asalkan relevan dengan user
-URL --> http://localhost:3000/cities
-menampilkan data cities berupa object.
-silakan menentukan property dan value dari object cities juga untuk release 0 ini.
+const express = require('express')
+const ejs = require('ejs')
+const bodyParser = require('body-parser');
 
-- Release 1
-Buatlah file data.json yang isinya ada 2 object yaitu users dan cities.
-contoh :
-{
-  users: [{
-    username: 'hacktiv8',
-    password: 'hacktiv8',
-    email: 'hactiv8@hacktiv8.com'
-  }, {
-    username: 'johndoe',
-    password: '12345',
-    email: 'john@doe.com'
-  }],
-  cities: [{
-    name: 'Jakarta'
-    province: 'DKI Jakarta'
-  }, {
-    name: 'Bandung'
-    province: 'Jawa Barat'
-  }]
-}
-Setelah itu, buatlah code untuk membaca file dari data.json
-sehingga routing yang tadinya menampilkan object yang dibuat sendiri,
-sekarang menampilkan data yang ada di-file data.json menggunakan view engine .ejs
+const app = express()
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-Release 2
-Buatlah routing untuk add, edit dan delete untuk /users dan /cities
-contoh:
-URL --> http://localhost:3000/users/add (untuk routing add users)
-URL --> http://localhost:3000/users/edit/:id (untuk routing edit users dengan mengirimkan id data)
-URL --> http://localhost:3000/users/delete/:id (untuk routing delete users dengan mengirimkan id data)
-**/
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+// Routing
+app.get('/', function (req, res) {
+  res.render('home', {nama: 'wisnu dj'})
+})
+
+app.get('/users', function(req, res) {
+  res.render('users', {username: 'hacktiv8', password: 'hacktiv8', email: 'hactiv8@hacktiv8.com'})
+})
+
+app.get('/cities', function(req, res) {
+  res.render('cities', {name: 'Jakarta', provience: 'DKI Jakarta'})
+})
+
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+})
