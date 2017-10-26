1bc2c748b6e234c8c1128fb3f243a14bd8c631d4
rezaalfarabi <anangreza18@gmail.com>
2017-10-27 00:49:25 +0700

    release0

--- app.js
+++ app.js
@@ -1,46 +1,64 @@
-/** EXPRESS FROM SCRATCH V.0
----------------------------
-Buatlah sebuah aplikasi sederhana menggunakan Express JS
-- Release 0
-Buatlah 3 routing yang memenuhi spesifikasi berikut ini :
-URL --> http://localhost:3000/
-menampilkan "Welcome to Express My App [Nama_Kalian]"
-URL --> http://localhost:3000/users
-menampilkan data users berupa object. di release ini object dibuat manual,
-silakan menentukan property dan value dari object tersebut asalkan relevan dengan user
-URL --> http://localhost:3000/cities
-menampilkan data cities berupa object.
-silakan menentukan property dan value dari object cities juga untuk release 0 ini.
 
-- Release 1
-Buatlah file data.json yang isinya ada 2 object yaitu users dan cities.
-contoh :
-{
-  users: [{
-    username: 'hacktiv8',
-    password: 'hacktiv8',
-    email: 'hactiv8@hacktiv8.com'
-  }, {
-    username: 'johndoe',
-    password: '12345',
-    email: 'john@doe.com'
-  }],
-  cities: [{
-    name: 'Jakarta'
-    province: 'DKI Jakarta'
-  }, {
-    name: 'Bandung'
-    province: 'Jawa Barat'
-  }]
-}
-Setelah itu, buatlah code untuk membaca file dari data.json
-sehingga routing yang tadinya menampilkan object yang dibuat sendiri,
-sekarang menampilkan data yang ada di-file data.json menggunakan view engine .ejs
+const express = require('express')
+const app = express()
 
-Release 2
-Buatlah routing untuk add, edit dan delete untuk /users dan /cities
-contoh:
-URL --> http://localhost:3000/users/add (untuk routing add users)
-URL --> http://localhost:3000/users/edit/:id (untuk routing edit users dengan mengirimkan id data)
-URL --> http://localhost:3000/users/delete/:id (untuk routing delete users dengan mengirimkan id data)
-**/
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/users', function (req, res) {
+  let obj = {
+    users: [{
+      username: 'hacktiv8',
+      password: 'hacktiv8',
+      email: 'hactiv8@hacktiv8.com'
+    }, {
+      username: 'johndoe',
+      password: '12345',
+      email: 'john@doe.com'
+    }],
+    cities: [{
+      name: 'Jakarta',
+      province: 'DKI Jakarta'
+    }, {
+      name: 'Bandung',
+      province: 'Jawa Barat'
+    }]
+  }
+  res.render('users', obj)
+  
+})
+
+app.get('/cities', function (req, res) {
+  let obj = {
+    cities: [{
+      id: '1',
+      username: 'hacktiv8',
+      password: 'hacktiv8',
+      email: 'hactiv8@hacktiv8.com'
+    }, {
+      id:'2',
+      username: 'johndoe',
+      password: '12345',
+      email: 'john@doe.com'
+    }],
+    cities: [{
+      id:'1',
+      name: 'tangerang',
+      province: 'Banten'
+    }, {
+      id:'2',
+      name: 'semarang',
+      province: 'Jawa tengah'
+    }]
+  }
+  res.render('cities', obj)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
